68f1d0e189b46c8937b9fb03595210f5617a56ef
Andre Le <dre@Andres-MBP-35.home>
2020-08-30 18:24:36 -0400

    added styling to app and got champion cards to populate

--- lol-app/app.js
+++ lol-app/app.js
@@ -1,16 +1,25 @@
-// $(() => {
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// })
+$(() => {
 
-console.log('hi');
+  $.ajax({ url: 'https://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion.json'
+}).then(
+  (data) => {
+    console.log(data.data);
+    for (let key in data.data) {
+    const $champDiv = $('<div>').addClass('champion')
+    const $champImg = $('<img>').addClass('champion-img')
+    const $nameDiv = $('<div>').addClass('name')
+    const champImg = 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/' + data.data[key].id + '_0.jpg'
+
+    $nameDiv.text(data.data[key].name)
+    $champImg.attr('src', champImg)
+
+    $champDiv.append($nameDiv, $champImg)
+    $('.champion-cards').append($champDiv)
+    }
+  },
+  (error) => {
+    console.log('doesn\'t work');
+  }
+)
+
+})
