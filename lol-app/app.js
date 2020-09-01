8091de0558dca4a8fa2934478758499994699ee6
Andre Le <dre@Andres-MBP-35.home>
2020-09-01 18:23:23 -0400

    added close functionality to modal

--- lol-app/app.js
+++ lol-app/app.js
@@ -3,7 +3,7 @@ $(() => {
   $.ajax({ url: 'https://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion.json'
 }).then(
   (data) => {
-    console.log(data.data);
+    // console.log(data.data);
     for (let key in data.data) {
     const $champDiv = $('<div>').addClass('champion')
     const $champImg = $('<img>').addClass('champion-img')
@@ -16,14 +16,16 @@ $(() => {
     const $modalp1 = $('<p>').attr('id', 'attack')
     const $modalp2 = $('<p>').attr('id', 'defence')
     const $modalp3 = $('<p>').attr('id', 'difficulty')
+    const $modalp4 = $('<p>').attr('id', 'type')
     const champImg = 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/' + data.data[key].id + '_0.jpg'
     const champSplash = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + data.data[key].id + '_0.jpg'
 
     $modalp1.text('Attack: ' + data.data[key].info.attack)
     $modalp2.text('Defense: ' + data.data[key].info.defense)
     $modalp3.text('Difficulty: ' + data.data[key].info.difficulty)
+    $modalp4.text('Type: ' + data.data[key].tags)
 
-    $modalText.append($splashImg, $modalName, $modalTitle, $modalp1, $modalp2, $modalp3)
+    $modalText.append($splashImg, $modalName, $modalTitle, $modalp1, $modalp2, $modalp3, $modalp4)
     $splashImg.attr('src', champSplash)
 
     $modal.append($modalText)
@@ -32,19 +34,24 @@ $(() => {
 
     $champDiv.append($nameDiv, $champImg)
     $('.champion-cards').append($champDiv)
+    $modal.toggle()
 
-    // const closeModal = () => {
-    //   $modal.detach();
-    // }
+      $champDiv.on('click', event => {
+        $champDiv.append($modal)
+        $modal.toggle();
+      })
 
-    $champDiv.on('click', event => {
-      $champDiv.append($modal)
+    const submitArr = []
 
-      // window.on('click', event => {
-      //   if (event.target == modal) {
-      //     closeModal();
-      // })
-    })
+      $('form').on('submit', event => {
+        event.preventDefault();
+
+        const userInput = $('input[type="text"]').val();
+        // const data = data.data[key]. + userInput
+
+
+        $('form').trigger('reset');
+      })
 
     }
   },
